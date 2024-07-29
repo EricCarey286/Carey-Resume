cf99eaca8fc197234132bacb3532987ed707512d
EricCarey286 <ewc0707@gmail.com>
2024-07-29 10:05:51 -0400

    New Youtube Playlist Feature + Cleanup

---
+++ src/app/components/YoutubeList/YoutubeList.tsx
@@ -0,0 +1,55 @@
+import Section from "../Section/Section";
+import "./YoutubeList.css";
+
+const YOUTUBE_PLAYLIST_API =
+  "https://www.googleapis.com/youtube/v3/playlistItems";
+
+const headshot = "/images/headshot.jpg";
+
+async function getData() {
+  const res = await fetch(
+    `${YOUTUBE_PLAYLIST_API}?part=snippet&maxResults=10&playlistId=PLMvVU3l5gyKWcAGstohdAaMC6dPUttMIb&key=${process.env.YOUTUBE_API_KEY}`
+  );
+  return res.json();
+}
+
+export default async function YoutubeList() {
+  const data = await getData();
+  console.log(data);
+  return (
+    <Section
+      title="Youtube Top 10 of the Week"
+      heading="Youtube Top 10 of the Week"
+      id="youtube"
+    >
+      <p>Slide through to see my favorite youtube videos of the week! Courtsey of the YouTube API v3</p>
+      <div className="yt-slider-container">
+        <ul className="yt-slider">
+          {data.items.map((item: any) => {
+            const { id, snippet = {} } = item;
+            const { title, thumbnails = {}, resourceId } = snippet;
+            const { medium = {} } = thumbnails;
+            //console.log(`snippet =  ${JSON.stringify(snippet)}`)
+            return (
+              <li key={id} className="yt-list-item">
+                <a
+                  href={`https://www.youtube.com/watch?v=${resourceId.videoId}`}
+                  target="_blank"
+                >
+                  <p>
+                    <img
+                      className="yt-slider-img"
+                      src={medium.url}
+                      alt={title}
+                    />
+                  </p>
+                  <h3 className="yt-slider-title">{title}</h3>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </Section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-"use client"
-const headshot = "/images/headshot.jpg";
-
 import Header from "./components/Header/Header";
 //import NavBar from "./components/Nav/navBar";
 import Footer from "./components/Footer/Footer";
@@ -8,13 +5,15 @@ import ConceptList from "./components/Concepts/ConceptList";
 import Jobs from "./components/Jobs/Jobs";
 import Contact from "./components/Contact/Contact";
 //import ProjectSection from "./components/Projects/ProjectSection";
+import YoutubeList from "./components/YoutubeList/YoutubeList"
 
 export default function Home() {
   return (
     <>
-      <Header image={headshot} />
+      <Header />
       <ConceptList />
       <Jobs />
+      <YoutubeList />
       <Contact />
       <Footer />
     </>
